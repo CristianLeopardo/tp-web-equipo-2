2ff7f75619f7ca905d7251f5a971288c7425d0ce
CristianLeopardo <cristianleopardo@gmail.com>
2023-05-25 20:16:58 -0300

    V1.0

---
+++ TPWeb_equipo-2/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TPWeb_equipo_2.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Hola</h1>
+    <asp:GridView runat="server" ID="dgvArticulos" CssClass="table table-success table-striped"></asp:GridView>
+</asp:Content>
---
+++ TPWeb_equipo-2/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+
+namespace TPWeb_equipo_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private List<Articulos> ListaArticulos;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cargarInfo();
+        }
+
+        private void cargarInfo()
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            ListaArticulos = negocio.Listar();
+            dgvArticulos.DataSource = ListaArticulos;
+            dgvArticulos.DataBind();
+        }
+    }
+
+}
